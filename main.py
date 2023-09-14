e823814ba3c9d1ff71ff149b5b2b56fdeb444382
hxia5 <hxia5@illinois.edu>
2023-09-13 22:37:26 -0400

    a

--- main.py
+++ main.py
@@ -1,19 +1,80 @@
-"""
-Main cli or app entry point
-"""
+import pandas as pd
+import matplotlib.pyplot as plt
+from ydata_profiling import ProfileReport
+from datetime import datetime
+from sklearn.linear_model import LinearRegression
 
-from mylib.calculator import add
-import click
+def Describe(df): 
+    return df.describe() 
 
-#var=1;var=2
+def get_median(df):
+    return df.median()
 
-@click.command("add")
-@click.argument("a", type=int)
-@click.argument("b", type=int)
-def add_cli(a, b):
-    click.echo(add(a, b))
+def PlotHistOfTotalConfirmed(csv):
+    pd.set_option("display.max_columns", None)
+    general_df = pd.read_csv(csv)
+    plt.figure(figsize=(10, 6))
+    plt.hist(general_df["TotalConfirmed"], bins=20, edgecolor="black")
+    plt.title("TotalConfirmed Distribution")
+    plt.xlabel("TotalConfirmed")
+    plt.ylabel("Count")
+    plt.show() 
 
+def PlotHistTotalConfirmedVsDate(csv):
+    pd.set_option("display.max_columns", None)
+    general_df = pd.read_csv(csv)
+    plt.figure(figsize=(10, 6))
+    for i in range(44870):
+        general_df["Date"][i] = datetime.strptime(general_df["Date"][i], '%Y-%m-%d').date()
+    #print(type(general_df["Date"][0]))
+    df = general_df.groupby("Date").sum()
+    #print(df)
+    df2 =df.reset_index()
+    plt.bar(df2["Date"], df2["TotalConfirmed"])
+    plt.title("TotalConfirmed vs Date")
+    plt.xlabel("Date")
+    plt.ylabel("TotalConfirmed")
+    plt.show() 
 
-if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    add_cli()
+def PlotScatter(csv):
+    pd.set_option("display.max_columns", None)
+    general_df = pd.read_csv(csv)
+    plt.figure(figsize=(10, 6))
+    for i in range(44870):
+        general_df["Date"][i] = datetime.strptime(general_df["Date"][i], '%Y-%m-%d').date()
+    #print(type(general_df["Date"][0]))
+    df = general_df.groupby("Date").sum()
+    #print(df)
+    df2 =df.reset_index()
+    plt.scatter(df2["Date"], df2["TotalConfirmed"])
+    plt.title("TotalConfirmed vs Date")
+    plt.xlabel("Date")
+    plt.ylabel("TotalConfirmed")
+    plt.show() 
+
+def LROfTotalConfirmedVsDate(csv):
+    pd.set_option("display.max_columns", None)
+    general_df = pd.read_csv(csv)
+    plt.figure(figsize=(10, 6))
+    df = general_df.groupby("Date").sum()
+    df2 =df.reset_index()
+    x = []
+    a = 0
+    for i in range(1282):
+        x.append(a)
+        a = a + 1
+    df2["date_index"] = x
+    lr = LinearRegression()  # create object for the class
+    lr.fit(df2["date_index"].values.reshape(-1, 1), df2["TotalConfirmed"])  # perform linear regression
+    Y_pred = lr.predict(df2["date_index"].values.reshape(-1, 1))  # make predictions
+    plt.scatter(df2["date_index"].values.reshape(-1, 1), df2["TotalConfirmed"])
+    plt.plot(df2["date_index"].values.reshape(-1, 1), Y_pred, color='red')
+    plt.title("TotalConfirmed vs Date")
+    plt.xlabel("Date")
+    plt.ylabel("TotalConfirmed")
+    plt.show()
+
+def Summary(csv):
+    general_df = pd.read_csv(csv)
+    profile = ProfileReport(general_df, title="Profiling Report")
+    profile.to_file("profile.html")
--- test_main.py
+++ test_main.py
@@ -1,10 +1,45 @@
-"""
-Test goes here
+from main import (
+    Describe,
+    get_median,
+    PlotHistOfTotalConfirmed,
+    PlotHistTotalConfirmedVsDate,
+    PlotScatter,
+    LROfTotalConfirmedVsDate,
+    Summary,
+)
+import pandas as pd
+import matplotlib.pyplot as plt
+from ydata_profiling import ProfileReport
+from datetime import datetime
+from sklearn.linear_model import LinearRegression
 
-"""
+example_csv = "https://data.ca.gov/dataset/4a9a896a-e64e-48c2-bb35-5589f80e7c52/resource/5a3f496d-04be-4405-aea0-e83e26076efc/download/covid19dashboard.csv"
+data = pd.read_csv(example_csv)
 
-from mylib.calculator import add
+def test_descirbe():
+    data = pd.read_csv(example_csv)
+    result = Describe(data)
+    assert result.loc['mean', 'TotalConfirmed'] == 1638.599265
+    assert result.loc['mean', 'TotalDeaths'] == 5.782594
+    assert result.loc['std', 'TotalConfirmed'] == 1257.548628
+    assert result.loc['std', 'TotalDeaths'] == 7.762456
 
+def test_median():
+    data = pd.read_csv(example_csv)
+    assert get_median(data['Shape_Leng']) == 60896.5746
+    assert get_median(data['Shape_Area']) == 230060526.252
 
-def test_add():
-    assert add(1, 2) == 3
+def test_plot1():
+    PlotHistOfTotalConfirmed(example_csv)
+
+def test_plot2():
+    PlotHistTotalConfirmedVsDate(example_csv)
+
+def test_plot3():
+    PlotScatter(example_csv)
+
+def test_plot4():
+    LROfTotalConfirmedVsDate(example_csv)
+
+def test_generate_summary_report():
+    Summary(example_csv)
